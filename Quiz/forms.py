21e81910371b3aa17a676317745a12d6c2410d73
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-02-16 09:06:51 +0100

    Update forms.py

--- Quiz/forms.py
+++ Quiz/forms.py
@@ -12,19 +12,16 @@ User = get_user_model()
 class ElegirInlineFormset(forms.BaseInlineFormSet):
 	def clean(self):
 		super(ElegirInlineFormset, self).clean()
-
 		respuesta_correcta = 0
 		for formulario in self.forms:
 			if not formulario.is_valid():
 				return
-
 			if formulario.cleaned_data and formulario.cleaned_data.get('correcta') is True:
 				respuesta_correcta += 1
-
 		try:
-			assert respuesta_correcta == Pregunta.NUMER_DE_RESPUESTAS_PERMITIDAS
+			assert respuesta_correcta == Pregunta.NUM_RESPUESTAS_PERMITIDAS
 		except AssertionError:
-			raise forms.ValidationError('Exactamente una sola respuesta es permitida')
+			raise forms.ValidationError(f'Se permite un total de: {Pregunta.NUM_RESPUESTAS_PERMITIDAS} respuesta/s')
 
 
 class UsuarioLoginFormulario(forms.Form):
